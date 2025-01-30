5952ff2275ef6e0b7304f42357a5bf0aded86c19
Somil Jain <somilj111@gmail.com>
2025-01-30 19:53:27 +0530

    first commit

---
+++ src/main/java/com/example/Rest_Api_Calling/RestApiCallingApplication.java
@@ -0,0 +1,13 @@
+package com.example.Rest_Api_Calling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCallingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCallingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Rest_Api_Calling/RestApiCallingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Rest_Api_Calling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCallingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
